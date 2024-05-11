69a4a475ce54b5aca0cd9042aeac09576c3bfeed
Alex-Kyryliuk <swerved104@gmail.com>
2024-05-11 16:00:40 +0300

    hw3 useStorage corrected the mistake

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
-import React, {useState} from 'react';
 import './App.css';
 import {useStorage} from "./hooks/CustomHooks";
 
 function App() {
-    const { save, isSave } = useStorage();
-    const [getSave, setGetSave] = useState(() => {
-        // @ts-ignore
-        return JSON.parse(localStorage.getItem('table')) || save;
-    });
-
-    const handleSaveClick = () => {
-        isSave(); // додав щоб прибрати TS6133: 'isSave' is declared but its value is never read.
-        const updatedSave = getSave + 1;
-        setGetSave(updatedSave);
-        localStorage.setItem("table", JSON.stringify(updatedSave));
-    };
+    const {getSave, handleSaveClick } = useStorage()
 
     return (
         <div>
--- src/hooks/CustomHooks.tsx
+++ src/hooks/CustomHooks.tsx
@@ -1,13 +1,23 @@
 // useStorage - hook that allows a component to store a value in the browser's LocalStorage
-
 import {useState} from "react";
 
 export const useStorage = ()=>{
     const [save, setSave] = useState<number>(0)
+    const [getSave, setGetSave] = useState(() => {
+        const saveStorage = localStorage.getItem('table');
+        return saveStorage ? JSON.parse(saveStorage) : save;
+    });
 
     const isSave = () => {
         setSave(save + 1)
     }
 
-    return {save, isSave};
+    const handleSaveClick = () => {
+        const updatedSave = getSave + 1;
+        setGetSave(updatedSave);
+        localStorage.setItem("table", JSON.stringify(updatedSave));
+    };
+
+    return { save, isSave, getSave, handleSaveClick };
+
 }

db88c441c26b610c9eae02448878bd04882498d5
Alex-Kyryliuk <swerved104@gmail.com>
2024-05-25 15:20:02 +0300

    hw 5 hw 6

--- src/components/users/UserComponent.tsx
+++ src/components/users/UserComponent.tsx
@@ -10,10 +10,14 @@ const UserComponent:FC<IProps> = ({user}) => {
     return (
         <div className={css.User}>
             <div>
-                Id: {user.id},
-                name: {user.name},
-                username: {user.username},
-                email: {user.email}
+                Id: {user.id};
+                name: {user.name};
+                username: {user.username};
+                email: {user.email};
+                address: street - {user.address.street}, suite - {user.address.suite}, city - {user.address.city}, zipcode - {user.address.zipcode}, geo: lat - {user.address.geo.lat}, lng - {user.address.geo.lng};
+                phone: {user.phone};
+                website: {user.website};
+                company: name - {user.company.name}, catchPhrase: {user.company.catchPhrase}, bs: {user.company.bs}
             </div>
 
             <NavLink to={user.id.toString()} state={{user: 'user'}}>
